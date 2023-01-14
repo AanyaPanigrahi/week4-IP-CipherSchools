b2bf7a2b9c0d0b1726ac6a4dfa066c73be4f0c3f
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-01-14 23:54:33 +0530

    Add files via upload

---
+++ week 4 IP/class 14,15,16,17,18/object.js
@@ -0,0 +1,66 @@
+// objects
+
+let obj = { name: "Cipher" };
+// console.log(obj);
+
+// person['country'] = 'India'   //  Wrong
+
+let person = {
+    name: 'John',
+    email: 'john@wich.com',
+    age: 45,
+    status: true,
+    children: {
+        name: 'Jay'
+    },
+    hobbies: ['Reading', 'Writing', true, 100, null]  //  Array
+}
+
+// let clickedName= 'age';
+// let clickedName= 'name';
+
+// let dynamicPropValue = 'age'
+
+// console.log(person[clickedName]);   //  !=== person.clickedName
+// console.log(person);
+
+// console.log(typeof person);
+
+// console.log(person.name);
+// console.log(typeof person.name);
+// console.log(person['age']);      // === person.age
+// console.log(person[children]);
+// console.log(person.clickedName);
+// console.log(person[dynamicPropValue]);
+// console.log(person.dynamicPropValue);
+
+// person.country = 'India'; 
+// console.log(person);
+
+// console.log(person);
+// console.log(year);
+
+let year = 2020
+
+console.log(year);
+console.log(person);
+person['country'] = 'India'
+
+// console.log(person);
+// console.log(person.hobbies);
+// console.log(null);
+
+/**
+ * Primitive Data 
+ * Number 
+ * String
+ * Boolean
+ * Undefined 
+*/
+
+/**
+ * Referene Datatype
+ * Object
+ * Arrays
+ * Function
+*/
