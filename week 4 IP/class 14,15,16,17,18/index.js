813bca01b1a3871062a46eb8274d477dc3018add
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-01-14 23:53:30 +0530

    Add files via upload

---
+++ week 4 IP/class 14,15,16,17,18/index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>JS</title>
+        <!-- <script>
+            console.log("My console statement!")
+        </script> -->
+    </head>
+    <body>
+        <header>
+            <h2>JavaScript is awesome but when u practice it.</h2>
+        </header>
+        <script src="./main.js"></script>
+        <script src="./objects.js"></script>
+        <script src="./function.js"></script>
+        <script src="./classes.js"></script>
+    </body>
+   
+</html>
